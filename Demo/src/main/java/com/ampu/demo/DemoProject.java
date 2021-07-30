b5a7eda9a3a140104a8c1821e48ab39e8e5e6ee5
Ampu <ampusasidharan@gmail.com>
2021-07-30 07:49:24 +0530

    Latest Commit

--- Demo/src/main/java/com/ampu/demo/DemoProject.java
+++ Demo/src/main/java/com/ampu/demo/DemoProject.java
@@ -5,6 +5,7 @@ public class DemoProject {
 	public static void main(String[] args) {
 		System.out.println("Hello USA");
 		System.out.println("Hello India");
+		System.out.println("Hello UK");
 
 	}
 
