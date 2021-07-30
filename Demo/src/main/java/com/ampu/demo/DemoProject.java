79ecfc80d1b4af00383d3da4ff5a048b49d1be0a
Ampu <ampusasidharan@gmail.com>
2021-07-30 07:29:47 +0530

    update java code 1

---
+++ Demo/src/main/java/com/ampu/demo/DemoProject.java
@@ -0,0 +1,11 @@
+package com.ampu.demo;
+
+public class DemoProject {
+
+	public static void main(String[] args) {
+		System.out.println("Hello USA");
+		System.out.println("Hello India");
+
+	}
+
+}
